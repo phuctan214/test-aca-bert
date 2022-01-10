4c4ae57b61b81e69f54ed9cb1bc2ddfc6c434281
Admin <phuctan214@gmail.com>
2022-01-10 20:27:02 +0700

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
-FROM nvcr.io/nvidia/pytorch:20.10-py3
+FROM nvcr.io/nvidia/pytorch:21.02-py3
 ARG DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update --fix-missing
 
-RUN useradd -m tan
+# RUN useradd -m tan
 
-RUN chown -R tan:tan /home/tan/
+# RUN chown -R tan:tan /home/tan/
 
-COPY --chown=tan . /home/tan/budget_bert/
+# COPY --chown=tan . /home/tan/budget_bert/
 
-USER tan
+COPY . /home/tan/budget_bert/
 
-RUN cd /home/tan/budget_bert/ && pip install -r requirements.txt
+# USER tan
+
+RUN cd /home/tan/budget_bert/ && pip3 install -r requirements.txt
+
+# RUN cd /home/tan/budget_bert/apex && pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 
 WORKDIR /home/tan/budget_bert/
--- dataset/run_data.sh
+++ dataset/run_data.sh
@@ -1,22 +1,22 @@
-# python shard_data.py \
-#     --dir /Users/tannp3.aic/Transformer-MGK/dataset \
-#     -o /Users/tannp3.aic/Transformer-MGK/dataset/shard \
-#     --num_train_shards 256 \
-#     --num_test_shards 128 \
-#     --frac_test 0.1
+python shard_data.py \
+     --dir /home/tan/budget_bert/dataset_corpus_vnese \
+     -o /home/tan/budget_bert/dataset_corpus_vnese/shard \
+     --num_train_shards 256 \
+     --num_test_shards 128 \
+     --frac_test 0.1
 
-#python generate_samples.py \
-#    --dir /Users/tannp3.aic/Transformer-MGK/dataset/shard \
-#    -o /Users/tannp3.aic/Transformer-MGK/dataset/hdf5/ \
-#    --dup_factor 10 \
-#    --seed 42 \
-#    --vocab_file None \
-#    --do_lower_case 1 \
-#    --masked_lm_prob 0.15\
-#    --model_name vinai/phobert-base \
-#    --max_predictions_per_seq 20 \
-#    --n_processes 16\
-#    --max_seq_length 128 \
+python generate_samples.py \
+    --dir /home/tan/budget_bert/dataset_corpus_vnese/shard \
+    -o /home/tan/budget_bert/dataset_corpus_vnese/hdf5 \
+    --dup_factor 10 \
+    --seed 42 \
+    --vocab_file None \
+    --do_lower_case 1 \
+    --masked_lm_prob 0.15\
+    --model_name /home/tan/budget_bert/phobert-base \
+    --max_predictions_per_seq 20 \
+    --n_processes 16\
+    --max_seq_length 128 \
 
 # python generate_samples.py \
 #     --dir <path_to_shards> \
